a383a024440cc2e3145a92542b6b66732e636324
suyuan <175338101@qq.com>
2025-03-27 00:16:27 +0800

    fiox

--- m5stickc-idf-master/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Main Makefile. This is basically the same as a component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-COMPONENT_SRCDIRS := . ./util ./util/font ./backported/esp_event ./backported/newlib
-COMPONENT_ADD_INCLUDEDIRS := . ./include ./util
